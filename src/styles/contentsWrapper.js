79ad5ae87324da528f74a6159e6c2461c8d6d3b7
Beomsoo <44296964+k1a11220@users.noreply.github.com>
2021-08-07 21:17:02 +0900

    Change font size

--- src/styles/contentsWrapper.js
+++ src/styles/contentsWrapper.js
@@ -4,7 +4,14 @@ const ContentsWrapper = styled.section`
   padding-bottom: 64px;
 
   & h2 {
+    font-size: 28px;
     margin-bottom: 32px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.device.md}) {
+    & h2 {
+      font-size: 24px;
+    }
   }
 `;
 
